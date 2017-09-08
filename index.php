89f664c704d34ef29d54b8b58b594fa9dd3457a2
Yannick nguyen <yannicknguyen@hotmail.com>
2017-09-07 17:23:19 -0700

    Adding index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "I can't wait to finish this semester"
+        echo "I can't wait to finish this semester!"
         ?>
     </body>
 </html>
